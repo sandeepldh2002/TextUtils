36765716ab5a8f11a71b67d36f983ef8edc3a3d6
Sandeep <sandeepldh2020@gmail.com>
2023-08-24 17:25:45 +0530

    second commit

--- textutils/settings.py
+++ textutils/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-94%fiqk%)g53ngv04-281dp#wxwk-id0%hk2tz3*l4=1l4%qhf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
