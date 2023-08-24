c0419b04553f9205962713c2efc450e25095794e
Sandeep <sandeepldh2020@gmail.com>
2023-08-24 22:32:52 +0530

    done vercel setting6

--- textutils/settings.py
+++ textutils/settings.py
@@ -123,5 +123,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 import os
+STATIC_URL = 'static/',
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
